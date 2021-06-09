56dc5b512f68b91eb07a898423d3c8234bca6e24
YuliaGordienko <75683718+YuliaGordienko@users.noreply.github.com>
2021-06-10 01:42:19 +0300

    edit js fiches

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+import cards from '../menu.json';
+import templeCard from './templates/temple-card.hbs'
+function makeUpCards (obj) {
+    return obj.map(templeCard).join('')
+}
+
+const linkForRender = document.querySelector('.js-menu')
+linkForRender.insertAdjacentHTML('afterbegin', makeUpCards(cards))
+const changeBtn = document.querySelector('#theme-switch-toggle')
+console.dir(changeBtn)
+changeBtn.addEventListener('change', onInputChangeTheme)
+function onInputChangeTheme (event) {
+   const body = document.querySelector('body');
+   changeBtn.checked ? body.classList.add('dark-theme') : body.classList.remove('dark-theme')
+   
+}
