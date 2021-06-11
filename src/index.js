469938a7f4fc9827b297a47f6224a0c5b8a4881b
YuliaGordienko <75683718+YuliaGordienko@users.noreply.github.com>
2021-06-11 11:01:10 +0300

    finish with localstorage

--- src/index.js
+++ src/index.js
@@ -8,10 +8,26 @@ function makeUpCards (obj) {
 const linkForRender = document.querySelector('.js-menu')
 linkForRender.insertAdjacentHTML('afterbegin', makeUpCards(cards))
 const changeBtn = document.querySelector('#theme-switch-toggle')
-console.dir(changeBtn)
 changeBtn.addEventListener('change', onInputChangeTheme)
-function onInputChangeTheme (event) {
-   const body = document.querySelector('body');
-   changeBtn.checked ? body.classList.add('dark-theme') : body.classList.remove('dark-theme')
-   
+const body = document.querySelector('body');
+body.classList.add('light-theme')
+if (localStorage.getItem('theme') === 'dark-theme') {
+    body.classList.add('dark-theme')
+    localStorage.setItem('theme', 'dark-theme')
+    changeBtn.checked = true
 }
+function onInputChangeTheme(event) {
+    
+    if (changeBtn.checked) {
+        body.classList.add('dark-theme')
+        localStorage.setItem('theme', 'dark-theme')
+        
+    } else {
+        body.classList.remove('dark-theme')
+        localStorage.setItem('theme', 'light-theme')
+    }
+
+    
+}
+
+
